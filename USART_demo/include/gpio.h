f9eb5a8106662e55009675280b0bcb91aff563a8
PapillonAz <1065940593@q.com>
2023-10-26 16:06:04 +0800

    10.16

---
+++ USART_demo/include/gpio.h
@@ -0,0 +1,16 @@
+/*
+ * @Author: PapillonAz 1065940593@q.com
+ * @Date: 2023-10-25 22:21:06
+ * @LastEditors: PapillonAz 1065940593@q.com
+ * @LastEditTime: 2023-10-25 22:22:47
+ * @FilePath: /IWDG_demo/include/gpio.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+#include "main.h"
+
+void GPIO_Init();
+
+#endif
