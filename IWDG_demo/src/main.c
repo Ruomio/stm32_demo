9e7fd1eb48a7908fb6f9422e2d0b7ee1f529e682
PapillonAz <1065940593@q.com>
2023-10-26 15:04:48 +0800

    10.25

--- IWDG_demo/src/main.c
+++ IWDG_demo/src/main.c
@@ -2,7 +2,7 @@
  * @Author: PapillonAz 1065940593@q.com
  * @Date: 2023-10-25 20:56:10
  * @LastEditors: PapillonAz 1065940593@q.com
- * @LastEditTime: 2023-10-25 23:33:24
+ * @LastEditTime: 2023-10-26 14:54:12
  * @FilePath: /IWDG_demo/src/main.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,7 +19,6 @@ int main(){
 
     // clock
     SystemClock_Config();
-    USART1_UART_Init();
 
     // 外设
     GPIO_Init();
